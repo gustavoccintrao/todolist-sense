51911622a6fe66480d3bc3c46dbec1c0adbcd8a3
Gustavo Cintrão <gustavoccintrao@MacBook-Pro-de-Gustavo.local>
2021-03-03 04:26:24 -0300

    Protótipo Funcional 1

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,21 +1,27 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import '../index.css';
 import { DataContext } from './DataProvider';
 
 export default function CreateTask() {
     const [todos, setTodos] = useContext(DataContext);
     const [todoName, setTodoName] = useState('');
+    const todoInput = useRef();
 
     const addTodo = e => {
         e.preventDefault();
         setTodos([...todos, { name: todoName, complete: false }])
         setTodoName('');
+        todoInput.current.focus();
     }
+
+    useEffect(() => {
+        todoInput.current.focus();
+    }, [])
 
     return (
         <form autoComplete="off" onSubmit={addTodo}>
-            <input type="todos" name="todos" id="todos"
-                required placeholder="O que precisa ser feito?" value={todoName}
+            <input type="todos" name="todos" id="todos" ref={todoInput}
+                required placeholder="O que você precisa fazer hoje?" value={todoName}
                 onChange={e => setTodoName(e.target.value.toLowerCase())} />
             <button type="submit">Criar</button>
         </form>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { DataContext } from './DataProvider'
 
 export default function Footer() {
+    const [checkAll, setCheckAll] = useState(false)
+    const [todos, setTodos] = useContext(DataContext)
+
+    const handleCheckAll = () => {
+        const newTodos = [...todos]
+        newTodos.forEach(todo => {
+            todo.complete = !checkAll
+        })
+        setTodos(newTodos)
+        setCheckAll(!checkAll)
+    }
+
+    const newTodosComplete = () => {
+        return todos.filter(todo => todo.complete === false)
+    }
+
+    const deleteTodo = () => {
+        setTodos(newTodosComplete())
+        setCheckAll(false)
+    }
+
     return (
-        <div className="row">
-            <label htmlFor="all">
-                <input type="checkbox" name="all" id="all" />
+        <>
+            { todos.length === 0 ? <h2> Parabéns! Nenhuma tarefa para fazer!</h2>
+                : <div className="row">
+                    <label htmlFor="all">
+                        <input type="checkbox" name="all" id="all" onChange={handleCheckAll} checked={checkAll} />
         Selecionar Tudo
         </label>
-            <p> Você tem 0 tarefas</p>
-            <button id="delete">Deletar</button>
+                    <p> Você tem {newTodosComplete().length} tarefas para fazer </p>
+                    <button id="delete" onClick={deleteTodo}>Deletar</button>
 
-        </div>
+                </div>
+            }
+        </>
     )
 }
